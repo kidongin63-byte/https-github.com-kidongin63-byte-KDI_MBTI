edfdf1afe8ee12cf8639e3c34a8b471272200725
kidong <inkidong26@gmail.com>
2026-07-12 14:12:18 +0900

    style: reduce all font size scaling settings by 20%

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,15 @@ export default {
     theme: {
         extend: {
             fontSize: {
-                'xs': ['1.3rem', { lineHeight: '1.6rem' }],
-                'sm': ['1.45rem', { lineHeight: '1.8rem' }],
-                'base': ['1.65rem', { lineHeight: '2.1rem' }],
-                'lg': ['1.85rem', { lineHeight: '2.3rem' }],
-                'xl': ['2.1rem', { lineHeight: '2.6rem' }],
-                '2xl': ['2.35rem', { lineHeight: '2.8rem' }],
-                '3xl': ['2.7rem', { lineHeight: '3.2rem' }],
-                '4xl': ['3.3rem', { lineHeight: '3.8rem' }],
-                '5xl': ['4.2rem', { lineHeight: '4.8rem' }],
+                'xs': ['1.1rem', { lineHeight: '1.4rem' }],
+                'sm': ['1.2rem', { lineHeight: '1.5rem' }],
+                'base': ['1.35rem', { lineHeight: '1.7rem' }],
+                'lg': ['1.5rem', { lineHeight: '1.9rem' }],
+                'xl': ['1.7rem', { lineHeight: '2.1rem' }],
+                '2xl': ['1.9rem', { lineHeight: '2.3rem' }],
+                '3xl': ['2.2rem', { lineHeight: '2.6rem' }],
+                '4xl': ['2.7rem', { lineHeight: '3.1rem' }],
+                '5xl': ['3.4rem', { lineHeight: '3.9rem' }],
             }
         },
     },
